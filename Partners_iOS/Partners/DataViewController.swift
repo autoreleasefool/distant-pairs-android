4fb4da2ce284e3adfafceb8a347f45a17d336524
Joseph Roque <github@josephroque.ca>
2015-07-01 16:48:20 -0400

    Created iphone project
    Created a new XCode project for the iOS app and renamed the Android and CloudCode directories

---
+++ Partners_iOS/Partners/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Partners
+//
+//  Created by Joseph Roque on 2015-07-01.
+//  Copyright (c) 2015 Joseph Roque. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
