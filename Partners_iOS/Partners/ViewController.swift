9237201b6863babb85c502580a35322f662da0ca
Joseph Roque <github@josephroque.ca>
2015-07-09 08:17:54 -0400

    Added new xcode project
    Decided to use xcode for first iphone app instead of defaulting to jetbrains ide

---
+++ Partners_iOS/Partners/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Partners
+//
+//  Created by Joseph Roque on 2015-07-09.
+//  Copyright (c) 2015 Joseph Roque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
